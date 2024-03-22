f48f718a8aac992387dd2d6725b915a81ae385d5
kalyani <Kalyani22@navgurukul.org>
2024-03-22 15:29:47 +0530

    crash is resolved

--- app/src/main/java/com/example/myapplication/apidata/Repository.kt
+++ app/src/main/java/com/example/myapplication/apidata/Repository.kt
@@ -1,22 +1,35 @@
 package com.example.myapplication.apidata
 
+import android.util.Log
 import com.example.myapplication.dataclasses.itemDataclass
 import com.example.myapplication.db.Daoclas
 
 class Repository(private val apiService: ApiService, private val itemDao: Daoclas) {
     suspend fun fetchDataAndInsertIntoDb() {
-        val response = apiService.getPathways()
-        if (response.isSuccessful) {
-            val pathways = response.body()?.pathways ?: emptyList()
-   ;         val courses = pathways.flatMap { pathway -> pathway.courses }
-            val items = courses.map { course ->
-                itemDataclass(title = course.name, logo = course.logo.toString(), name = "", courses = listOf(course))
+        try {
+            val response = apiService.getPathways()
+            if (response.isSuccessful) {
+                val pathways = response.body()?.pathways ?: emptyList()
+                ;
+                val courses = pathways.flatMap { pathway -> pathway.courses }
+                val items = courses.map { course ->
+                    itemDataclass(
+                        title = course.name,
+                        logo = course.logo.toString(),
+                        name = "",
+                        courses = listOf(course)
+                    )
+                }
+                itemDao.insertAll(items)
+            } else {
+                Log.e("Repository", "fetchDataAndInsertIntoDb: ${response.errorBody()}")
             }
-            itemDao.insertAll(items)
-        } else {
-            // Handle error].
+        }
+        catch (e: Exception) {
+            Log.e("Repository", "fetchDataAndInsertIntoDb: ${e.message}")
         }
     }
 }
+
 
 
